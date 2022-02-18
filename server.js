1bc005475cb2a1f9d0f0b349195ade0c2abe2016
PamelaCleveland <pamelac021@gmail.com>
2022-02-18 02:45:41 -0500

    query db to test connection

--- server.js
+++ server.js
@@ -30,6 +30,10 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
   );
 
+  db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+  });
+
 // Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
