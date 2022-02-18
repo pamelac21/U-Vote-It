58e44b5f48822429399e2a76145fbfaeae6baa9d
PamelaCleveland <pamelac021@gmail.com>
2022-02-18 03:01:32 -0500

    mysql queries test

--- server.js
+++ server.js
@@ -8,14 +8,14 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Test the Express.js connection (below)
 /*
+// Test the Express.js connection (below)
 app.get('/', (req, res) => {
     res.json({
       message: 'Hello World'
     });
   });
-  */
+*/
 
 // Connect to database
 const db = mysql.createConnection(
@@ -29,11 +29,40 @@ const db = mysql.createConnection(
     },
     console.log('Connected to the election database.')
   );
-
+/*
   // Query db to test mysql connection
   db.query(`SELECT * FROM candidates`, (err, rows) => {
     console.log(rows);
   });
+*/ /*
+// GET a single candidate
+db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(row);
+  });
+*/ /*
+// Delete a candidate
+db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(result);
+  });
+*/ /*
+// Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+              VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result);
+});
+*/
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
